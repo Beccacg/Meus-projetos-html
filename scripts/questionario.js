bf5205694dbc039a5e1c9c81ad9fb9d00da5a107
Rebeca Goldoni <carvalhorebeca340@Gmail.com>
2023-05-16 22:16:52 -0400

    new

---
+++ scripts/questionario.js
@@ -0,0 +1,68 @@
+/*
+var n1= parseInt(prompt('Informe o primeiro valor'));
+var n2= parseInt(prompt('Informe o segundo valor'));
+var n3= parseInt(prompt('Informe o terceiro valor'));
+var n4= parseInt(prompt('Informe o quarto valor'));
+
+alert((n1+n2+n3+n4)/4);
+*/
+
+/*
+var saldo= parseFloat(prompt('Informe o se saldo'));
+
+alert("Saldo reajustado para 2%:" + saldo*1.02);
+
+*/
+
+/*
+var n1= parseInt(prompt('Informe um valor em segundos:'));
+
+var resultado = Math.floor(n1/60);
+
+alert("Seus segundos convertidos em minutos são:" + resultado+ "minutos e "+n1%60+" segundos");
+*/
+
+/*
+var n1=parseInt(prompt('Informe um valor em minutos:'));
+
+var horas = Math.floor(n1/60);
+
+var dia = Math.floor(horas/1440);
+
+var hora = Math.floor(horas%24);
+
+var minuto=(n1-((dia*1440)+(hora*60)));
+
+document.write("<h1>Seus minutos convertidos em dias são: "+dia+" dias "+hora+" hora "+minuto+" minuto "</h1>);
+
+*/ 5
+/*
+var dias = parseInt(promtp("Informe os dias:"));
+var horas = parseInt(promtp("Informe as horas:"));
+var minuto = ((dia*1440)+(hora+60));
+
+document.write('Valor em minutos:')
+
+*/
+
+/* 6
+
+var raio = parseInt(prompt("Informe o raio da cincuferencia:"))
+
+var pi = Math.PI;
+
+document.write('Circunferencia:'+2*raio*pi);
+*/
+
+var polegada = parseInt(prompt("Informe as polegadas:"));
+
+document.write('Milimetros: '+ polegadas*25.4);
+
+
+
+
+
+
+
+
+
